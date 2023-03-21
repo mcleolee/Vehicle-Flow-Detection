4bf2fe922788dbdd460a602c4462c11a7dc90049
Raven <1772133019@qq.com>
2023-03-21 23:53:13 +0800

    2353

--- cardetc.h
+++ cardetc.h
@@ -12,14 +12,12 @@ using std::cin;
 
 class Asset{
 public:
-
-
-
     // 判断 asset 是不是图像
     bool isImage(const std::string &filename);
-
-// 判断 asset 是不是视频
+    // 判断 asset 是不是视频
     bool isVideo(const std::string &filename);
+    // 测试
+    int testForImageAndVideo();
 
 };
 
@@ -45,7 +43,22 @@ inline bool Asset::isVideo(const std::string &filename)
     return false;
 }
 
+inline int Asset::testForImageAndVideo(){
+    std::string testString1 = "fuckme.jpg";
+    std::string testString2 = "fuckme.mp4";
+    bool test1 = isImage(testString1);
+    bool test2 = isVideo(testString1);
+    bool test3 = isImage(testString2);
+    bool test4 = isVideo(testString2);
+    log("the result should be 1 0 0 1");
+    log(test1);
+    log(test2);
+    log(test3);
+    log(test4);
+}
 
+
+// 写一个把视频分解成连续的图片的类
 
 
 #endif //VEHICLE_FLOW_DETECTION_CARDETC_H
--- main.cpp
+++ main.cpp
@@ -2,12 +2,9 @@
 #include "cardetc.h"
 
 int main() {
-    std::string testString1 = "fuckme.jpg";
-    std::string testString2 = "fuckme.mp4";
     Asset ast;
-    bool test1 = ast.isImage(testString1);
+    ast.testForImageAndVideo();
 
-    log(test1);
 
     return 0;
 }
